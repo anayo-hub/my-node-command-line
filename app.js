74ad765f6ac08772a486af8856314168ad5a2689
Criss Ayo <anayocrescent2@gmail.com>
2023-08-09 10:44:51 +0100

    add EventEmitter

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const fs = require("fs/promises");
 
 (async () => {
-  const CLI = fs.open("./command.txt", "r");
-  const watcher = fs.watch("./command.txt");
+  try {
+    // oepn mmethod return another method calleed filehandler
+    const CFH = await fs.open("./command.txt", "r");
 
-  for await (const { eventType } of watcher) {
-    if (eventType) {
-      console.log(eventType);
+    CFH.on("change", async () => {
+      //check size to allocate buff
+      const size = (await CFH.stat()).size;
+      const buff = Buffer.alloc(size);
+
+      const offfset = 0;
+      const length = buff.byteLength;
+      const position = 0;
+
+      const content = await CFH.read(buff, offfset, length, position);
+      console.log(content);
+    });
+
+    //watcher over ffile
+    const watcher = fs.watch("./command.txt");
+    //loop ovr file  to get event
+    for await (const event of watcher) {
+      // fs.watch returns only change evt
+      if (event.eventType === "change") {
+        CFH.emit("change");
+      }
+    }
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      console.log("file does not exist, please check your file name");
+    } else {
+      console.log(error);
     }
   }
 })();
--- test.js
+++ test.js
@@ -1,50 +1,75 @@
-// first
-fs.watch("./", { recursive: true }, (eventType, filename) => {
-  try {
-    if (filename) {
-      console.log(`Event type: ${eventType} - File: ${filename}`);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
+// // first
+// fs.watch("./", { recursive: true }, (eventType, filename) => {
+//   try {
+//     if (filename) {
+//       console.log(`Event type: ${eventType} - File: ${filename}`);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
-// second
+// // second
+// const fs = require("fs/promises");
+
+// (async () => {
+//   fs.watch("./", { recursive: true }, (eventType, filename) => {
+//     try {
+//       if (filename) {
+//         console.log(`Event type: ${eventType} - File: ${filename}`);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
+// })();
+
+// // third
+// const fs = require("fs");
+
+// fs.watch("./", { recursive: true }, (eventType, filename) => {
+//   try {
+//     if (filename) {
+//       console.log(`Event type: ${eventType} - File: ${filename}`);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
+
+// const fs = require("fs/promises");
+
+// (async () => {
+//   const watcher = fs.watch("./command.txt");
+
+//   for await (const { eventType, filename } of watcher) {
+//     if (eventType === "change") {
+//       //   how do we read the content to determine the next action with using the file as our command line
+//     }
+//   }
+// })();
+
 const fs = require("fs/promises");
 
 (async () => {
-  fs.watch("./", { recursive: true }, (eventType, filename) => {
-    try {
-      if (filename) {
-        console.log(`Event type: ${eventType} - File: ${filename}`);
+  try {
+    // oepn mmethod return another method calleed filehandler
+    const CFH = await fs.open("./command.txt", "r");
+    //watcher over ffile
+    const watcher = fs.watch("./command.txt");
+    //loop ovr file  to get event
+    for await (const event of watcher) {
+      // fs.watch returns only change evt
+      if (event.eventType === "change") {
+        //check size to allocate buffer
+        console.log(await CFH.stat());
       }
-    } catch (error) {
-      console.log(error);
-    }
-  });
-})();
-
-// third
-const fs = require("fs");
-
-fs.watch("./", { recursive: true }, (eventType, filename) => {
-  try {
-    if (filename) {
-      console.log(`Event type: ${eventType} - File: ${filename}`);
     }
   } catch (error) {
-    console.log(error);
-  }
-});
-
-const fs = require("fs/promises");
-
-(async () => {
-  const watcher = fs.watch("./command.txt");
-
-  for await (const { eventType, filename } of watcher) {
-    if (eventType === "change") {
-      //   how do we read the content to determine the next action with using the file as our command line
+    if (error.code === "ENOENT") {
+      console.log("file does not exist, please check your file name");
+    } else {
+      console.log(error);
     }
   }
 })();
