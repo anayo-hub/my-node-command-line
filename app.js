f6d206c93f5168e952ffd6bfc9ba0492ed64826c
Criss Ayo <anayocrescent2@gmail.com>
2023-08-09 19:52:29 +0100

    add a path to read

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const fs = require("fs/promises");
 
 (async () => {
   try {
+    // commands
+    const create = "create a file";
+
+    const createFile = async (path) => {
+      let existingFileHandle;
+      try {
+        existingFileHandle = await fs.open(path, "r");
+        return console.log(`this ile ${path} already exists, use another name`);
+        fs.writeFile("text.txt", path);
+      } catch (error) {
+        console.log(error);
+      }
+      existingFileHandle.close();
+    };
+
     // oepn mmethod return another method calleed filehandler
     const CFH = await fs.open("./command.txt", "r");
 
@@ -14,11 +29,17 @@ const fs = require("fs/promises");
       const length = buff.byteLength;
       const position = 0;
 
-      const content = await CFH.read(buff, offfset, length, position);
-      console.log(content);
+      await CFH.read(buff, offfset, length, position);
+      const command = buff.toString("utf-8");
+
+      //create a flie
+      if (command.includes(create)) {
+        const filePath = command.substring(create + 1);
+        createFile(filePath);
+      }
     });
 
-    //watcher over ffile
+    //main watcher over file
     const watcher = fs.watch("./command.txt");
     //loop ovr file  to get event
     for await (const event of watcher) {
